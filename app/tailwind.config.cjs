3fa824eeb40f979b2ed4cda6237969b0f4a528c8
indexpro24@gmail.com <indexpro24@gmail.com>
2022-07-06 19:18:01 +0300

    git init

---
+++ app/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './node_modules/symax/**/*.{html,js,svelte,ts}',
+    './src/**/*.{html,js,svelte,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ]
+}
